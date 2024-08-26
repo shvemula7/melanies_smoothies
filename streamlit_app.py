c2fffa9054443d0fb9a33800f9d9d19d0d94e581
shvemula7 <shravanthi.vemula@bswhealth.org>
2024-08-26 14:50:14 -0500

    Update streamlit_app.py
    Fruityvice dataframework

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,8 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 
+#New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_responce.json(), use_container_width=True)
