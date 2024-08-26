7206893d5823e9cd6c71bf751989eed879b8ea3b
shvemula7 <shravanthi.vemula@bswhealth.org>
2024-08-26 15:58:28 -0500

    Update streamlit_app.py
    adding dataframe to test pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,13 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-#Fetch the available fruit options from database
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the Snowpark to a Pandas dataframe so we use the LOC funstion
+pf_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
